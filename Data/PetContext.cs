dfdd3bc23b2a3921ab3abfc6bf5cf9b66be07905
Ty Taylor <tdjtaylor18@gmail.com>
2025-12-27 19:55:37 -0500

    "Initial commit: ASP.NET Core Web API with EF Core, AnimalsController, PetContext, and Swagger setup"

---
+++ Data/PetContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FurAndFangs.Api.Models;
+
+namespace FurAndFangs.Api.Data
+{
+    public class PetContext : DbContext
+    {
+        public PetContext(DbContextOptions<PetContext> options)
+            : base(options)
+        {
+        }
+
+            public DbSet<Animal> Animals { get; set; }
+    }
+}
---
+++ Data/PetContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FurAndFangs.Api.Data
+{
+    public class PetContextFactory : IDesignTimeDbContextFactory<PetContext>
+    {
+        public PetContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PetContext>();
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=FurAndFangsDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new PetContext(optionsBuilder.Options);
+        }
+    }
+}
