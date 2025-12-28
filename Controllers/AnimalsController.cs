ac9aa6a4746293909635a0ecbd796bc3bd382cba
Ty Taylor <tdjtaylor18@gmail.com>
2025-12-27 19:55:37 -0500

    "Initial commit: ASP.NET Core Web API with EF Core, AnimalsController, PetContext, and Swagger setup"

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FurAndFangs.Api.Data;
+using FurAndFangs.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FurAndFangs.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly PetContext _context;
+        public AnimalsController(PetContext context)
+        {
+            _context = context;
+        }
+        // Get apt/animals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimals()
+        {
+            return await _context.Animals.ToListAsync();
+        }
+        // post api/animals
+        [HttpPost]
+        public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
+        {
+            _context.Animals.Add(animal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAnimals), new { id = animal.Id }, animal);
+        }
+    }
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FurAndFangs.Api.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; } // Cat,Dog, Reptile, etc.
+        public string Breed { get; set; } // Dog, Cat, Rabbit, etc.
+        public int Age { get; set; }
+        public string Sex { get; set; } // Male,Female,Unknown
+        public string Diet { get; set; } // Herbivore,Carnivore,Omnivore
+        public string Notes { get; set; }
+    }
+}
+
